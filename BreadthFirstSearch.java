eb71bbc1e8337980be5166f9548a3bfbe8daad31
lheritier.adam <lheritier.adam@gmail.com>
2021-11-17 12:01:18 +0100

    Initial commit

---
+++ BreadthFirstSearch.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class BreadthFirstSearch {
+
+    public static ArrayList<Arc> generateTree(Graph graph, int i) {
+        ArrayList<Arc> result = new ArrayList<>();
+        return result;
+    }
+}
---
+++ Pair.java
@@ -0,0 +1,17 @@
+import java.util.AbstractCollection;
+import java.util.AbstractList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class Pair<L,R> {
+    private L l;
+    private R r;
+    public Pair(L l, R r){
+        this.l = l;
+        this.r = r;
+    }
+    public L getL(){ return l; }
+    public R getR(){ return r; }
+    public void setL(L l){ this.l = l; }
+    public void setR(R r){ this.r = r; }
+}
